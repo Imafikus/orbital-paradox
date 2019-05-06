b1aeb9d1e0664182f9b5ab488804cc0bb1c18707
Aleksa Tesic <tesic@getpenta.com>
2019-05-06 11:36:05 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class OrbitalParadox():
+    def __init__(self):
+        pass
+
+
+
+
+
+if __name__ == "__main__":
+    op = OrbitalParadox()
